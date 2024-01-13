51e6434554bc3066dad186f23814ecbff70ab116
Nihaz <nihazasar@gmail.com>
2024-01-13 23:35:11 +0530

    Read write serializer added

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from tags.models import Tags
-class Tagserializer(serializers.ModelSerializer):
+
+#Serializer is used to convert orm data into json
+#read operation from the database
+class ReadTagserializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%d/%m/%y")
     class Meta:
         model = Tags
         fields = ['id', 'name', 'slug', 'created_at','updated_at']
+
+#Write operation into the database
+#Used for data validation and tag creation
+class WriteTagserializer(serializers.ModelSerializer):
+   #created_at = serializers.DateTimeField(format="%d/%m/%y")
+    class Meta:
+        model = Tags
+        fields = ['name']
--- tags/views.py
+++ tags/views.py
@@ -1,8 +1,30 @@
 #from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from tags.serializers import WriteTagserializer, ReadTagserializer
+from tags.models import Tags
+from django.utils.text import slugify
 class CreateTagView(APIView):
-    def get(self,request):
-        pass
+    def post(self,request):
+        #for write operation we use data=request.data(from client side validation)
+        serializer = WriteTagserializer(data=request.data)
+        if serializer.is_valid():
+            name =serializer.validated_data.get('name')
+            tag_object =Tags.objects.create(
+                name = name,
+                slug = slugify(name)
+            )
+            #Important note
+            #For read operatio n we use instance = tag_object
+            json_data = ReadTagserializer(instance=tag_object).data
+            return Response(json_data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 
 # Create your views here.
