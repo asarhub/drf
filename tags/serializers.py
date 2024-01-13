62a159be9a372506106249e775cb9fc71837f954
Nihaz <nihazasar@gmail.com>
2024-01-13 22:40:36 +0530

    Added time format representation code

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from tags.models import Tags
 class Tagserializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d/%m/%y")
     class Meta:
         model = Tags
         fields = ['id', 'name', 'slug', 'created_at','updated_at']
