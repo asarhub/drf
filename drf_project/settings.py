ba22c4857a50f5a837bfc9b18d8e7858b631c631
Nihaz <nihazasar@gmail.com>
2024-02-03 16:03:57 +0530

    Caching code added

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import redis
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#Used for caching
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
--- tags/views.py
+++ tags/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from tags.serializers import WriteTagserializer, ReadTagserializer
 from tags.models import Tags
 from django.utils.text import slugify
+from django.core.cache import cache
 class CreateTagView(APIView):
     def post(self,request):
         #for write operation we use data=request.data(from client side validation)
@@ -28,10 +29,21 @@ class CreateTagView(APIView):
 # Create your views here.
 class DetailTagView(APIView):
     #We will get the name of object based on slug information provided
+    #You can check it in postman where URL has {{LOCAL_HOST}}/tags/detail/slug_name
     def get(self, request, slug):
         try:
+            #This code is for caching,currently it is not working
+            #Enable all the code present below to check whether cache is working or not. When you are viewing detials for the first time it is cached next time it will come from cache
+            #cache_key = f"tags-{slug}"
+            #cached_data = cache.get(cache_key)
+            #if cached_data is not None:
+                #print("coming from cache")
+                #return Response(cached_data,status=status.HTTP_200_OK)
             tag_object = Tags.objects.get(slug=slug)
             response_data = ReadTagserializer(instance = tag_object).data
+            #print("Generating and caching the data")
+            #timeout is the time till when this data will be cached. It is in seconds format
+            #cache.set(cache_key,response_data,timeout=10)
             return Response(response_data, status=status.HTTP_200_OK)
         except Tags.DoesNotExist:
             return Response({"message":"Tag not found"},status=status.HTTP_404_NOT_FOUND)
